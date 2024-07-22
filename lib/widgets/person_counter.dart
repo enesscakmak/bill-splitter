6a7353ef4d6a6ef7385c1db724a4f1f882c7feb9
Enes Çakmak <cakmakkeness@gmail.com>
2024-07-22 20:34:47 +0300

    final refactor of person_counter

--- lib/widgets/person_counter.dart
+++ lib/widgets/person_counter.dart
@@ -5,14 +5,14 @@ class PersonCounter extends StatelessWidget {
     super.key,
     required this.theme,
     required int personCount,
-    required this.onIncrement,
     required this.onDecrement,
+    required this.onIncrement,
   }) : _personCount = personCount;
 
   final ThemeData theme;
   final int _personCount;
-  final VoidCallback? onIncrement;
-  final VoidCallback? onDecrement;
+  final VoidCallback onDecrement;
+  final VoidCallback onIncrement;
 
   @override
   Widget build(BuildContext context) {
@@ -20,25 +20,31 @@ class PersonCounter extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          "Split",
+          'Split',
           style: theme.textTheme.titleMedium,
         ),
         Row(
           children: [
             IconButton(
+              onPressed: onDecrement,
+              icon: Icon(
+                Icons.remove,
                 color: theme.colorScheme.primary,
-                onPressed: onDecrement,
-                icon: const Icon(Icons.remove)),
+              ),
+            ),
+            Text(
+              '$_personCount',
+              style: theme.textTheme.titleMedium,
+            ),
+            IconButton(
+              onPressed: onIncrement,
+              icon: Icon(
+                Icons.add,
+                color: theme.colorScheme.primary,
+              ),
+            ),
           ],
         ),
-        Text(
-          "$_personCount",
-          style: theme.textTheme.titleMedium,
-        ),
-        IconButton(
-            color: theme.colorScheme.primary,
-            onPressed: onIncrement,
-            icon: const Icon(Icons.add)),
       ],
     );
   }
